c6bb0f08afc6b445b440024ed4e2f80662c4881c
Varsha Elsa Jacob <varsha@Varshas-MacBook-Air.local>
2022-12-13 11:36:08 +0000

    this commit invoves changes in test and controller

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -13,19 +13,14 @@ describe('GET /api/topics task 3',() => {
     test('responds with status code 200',() => {
         return request(app).get('/api/topics')
         .expect(200)
-    });
-    test('respond body will contain an array of topic objects',() => {
-        return request(app).get('/api/topics')
         .then(({body}) => {
-            expect(Array.isArray(body.topics)).toBe(true);
             expect(body.topics.length).toBe(3);
-        });
-    });
-    test('respond topic will contain properties slug and description',() => {
-        return request(app).get('/api/topics')
-        .then(({body}) => {
-            body.topics.forEach((obj) => {
-                expect(Object.keys(obj).every(key => ['description','slug'].includes(key))).toEqual(true)
+            body.topics.forEach((topicObj) => {
+                expect.objectContaining({
+                    'slug': expect(typeof(topicObj['slug'])).toBe('string'),
+                    'description': expect(typeof(topicObj['description'])).toBe('string')
+                })
+                // expect(Object.keys(obj)).toMatchObject(['description','slug'])
             })
         });
     });
